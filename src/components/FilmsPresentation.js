3da503e5382c5e63344c7f750b2d7e016e8c4cdc
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-05-29 23:15:59 +0700

    Done layout

--- src/components/FilmsPresentation.js
+++ src/components/FilmsPresentation.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+
 export default function FilmsPresentation({ films }) {
+    const [selectedFilm, setSelectedFilm] = useState(null);
+
+    const handleOpenPopup = (film) => {
+        setSelectedFilm(film);
+    };
+
+    const handleClosePopup = () => {
+        setSelectedFilm(null);
+    };
+
     return (
-        <div className="container" id='Film'>
-            {films.map((films) => (
-                <div className="column">
-                    <div className="film-card" key={films.id}>
+        <div className="container" id="Film">
+            {films.map((film) => (
+                <div className="column" key={film.id}>
+                    <div className="film-card">
                         <div className="film-image">
-                            <img src={films.image} alt="" />
+                            <img src={film.image} alt="" />
                         </div>
                         <div className="film-details">
-                            <h2 className="film-title">{films.name}</h2>
-                            <p className="film-year">Year: {films.year}</p>
-                            <p className="film-nation">Nation: {films.nation}</p>
+                            <h2 className="film-title">{film.name}</h2>
+                            <p className="film-year">Year: {film.year}</p>
+                            <p className="film-nation">Nation: {film.nation}</p>
                         </div>
                         <div className="watch-film-btn">
-                            <button>Watch Film</button>
+                            <button onClick={() => handleOpenPopup(film)}>Detail</button>
                         </div>
                     </div>
                 </div>
             ))}
+
+            {selectedFilm && (
+                <div className="popup-container">
+                    <div className="popup-content">
+                        <button className="close-popup" onClick={handleClosePopup}>
+                            &times;
+                        </button>
+                        <div className="film-details-popup">
+                            <div className="film-image">
+                                <img src={selectedFilm.image} alt="" />
+                            </div>
+                            <div className="film-info">
+                                <h2>{selectedFilm.name}</h2>
+                                <p>Year: {selectedFilm.year}</p>
+                                <p>Nation: {selectedFilm.nation}</p>
+                                <p>Director: {selectedFilm.director}</p>
+                                <p>Description: {selectedFilm.description}</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
--- src/components/ListOfFilms.js
+++ src/components/ListOfFilms.js
@@ -1,12 +1,94 @@
 export const ListOfFilms = [
-    { id: 1, name: 'Star Wars: Episode IV - A New Hope', year: 1977, nation: 'USA', image: './assets/images/poster1.jpg' },
-    { id: 2, name: 'The Godfather', year: 1972, nation: 'USA', image: './assets/images/poster2.jpg' },
-    { id: 3, name: 'The Shawshank Redemption', year: 1994, nation: 'USA', image: './assets/images/poster3.jpg' },
-    { id: 4, name: 'Dreamy Eyes', year: 2018, nation: 'Viet Nam', image: './assets/images/poster4.jpg' },
-    { id: 5, name: 'The Dark Knight', year: 2008, nation: 'USA', image: './assets/images/poster5.jpg' },
-    { id: 6, name: 'End Game', year: 2019, nation: 'USA', image: './assets/images/poster6.jpg' },
-    { id: 7, name: 'Fast & Furious 8', year: 2017, nation: 'USA', image: './assets/images/poster7.jpg' },
-    { id: 8, name: 'Demons Slayer', year: 2020, nation: 'Japan', image: './assets/images/poster8.jpg' },
-    { id: 9, name: 'Inception', year: 2010, nation: 'USA', image: './assets/images/poster9.jpg' },
-    { id: 10, name: 'The Lord of the Rings: The Return of the King', year: 2003, nation: 'USA', image: './assets/images/poster10.jpg' },
+    {
+        id: 1,
+        name: 'Star Wars: Episode IV - A New Hope',
+        year: 1977,
+        nation: 'USA',
+        director: 'George Lucas',
+        image: './assets/images/poster1.jpg',
+        description: 'Star Wars: Episode IV - A New Hope is a science fiction film set in a galaxy far, far away. It follows the journey of Luke Skywalker as he joins forces with a group of rebels to fight against the evil Darth Vader and his empire.'
+    },
+    {
+        id: 2,
+        name: 'The Godfather',
+        year: 1972,
+        nation: 'USA',
+        director: 'Francis Ford Coppola',
+        image: './assets/images/poster2.jpg',
+        description: 'The Godfather is a crime film that tells the story of the Corleone crime family and their struggle to maintain their power and protect their interests. It is considered one of the greatest films of all time.'
+    },
+    {
+        id: 3,
+        name: 'The Shawshank Redemption',
+        year: 1994,
+        nation: 'USA',
+        director: 'Frank Darabont',
+        image: './assets/images/poster3.jpg',
+        description: 'The Shawshank Redemption is a drama film based on the Stephen King novella. It depicts the life of Andy Dufresne, a banker who is sentenced to life in Shawshank State Penitentiary and his friendship with a fellow inmate named Red.'
+    },
+    {
+        id: 4,
+        name: 'Dreamy Eyes',
+        year: 2018,
+        nation: 'Viet Nam',
+        director: 'Victor Vu',
+        image: './assets/images/poster4.jpg',
+        description: 'Dreamy Eyes is a Vietnamese romantic drama film that explores the complexities of love and relationships. It follows the journey of two individuals as they navigate their feelings and overcome obstacles to be together.'
+    },
+    {
+        id: 5,
+        name: 'The Dark Knight',
+        year: 2008,
+        nation: 'USA',
+        director: 'Christopher Nolan',
+        image: './assets/images/poster5.jpg',
+        description: 'The Dark Knight is a superhero film based on the DC Comics character Batman. It showcases Batman\'s efforts to protect Gotham City from the chaos unleashed by the Joker, a notorious supervillain.'
+    },
+    {
+        id: 6,
+        name: 'End Game',
+        year: 2019,
+        nation: 'USA',
+        director: 'Anthony Russo, Joe Russo',
+        image: './assets/images/poster6.jpg',
+        description: 'End Game is a superhero film and the epic conclusion to the Marvel Cinematic Universe\'s Infinity Saga. It brings together a wide array of Marvel superheroes as they strive to undo the damage caused by the villain Thanos.'
+    },
+    {
+        id: 7,
+        name: 'Fast & Furious 8',
+        year: 2017,
+        nation: 'USA',
+        director: 'F. Gary Gray',
+        image: './assets/images/poster7.jpg',
+        description: 'Fast & Furious 8 is an action film and the eighth installment in the Fast & Furious franchise. It follows Dominic Toretto and his team as they face off against a high-tech terrorist and his plans for world domination.'
+    },
+    {
+        id:
+
+            8,
+        name: 'Demon Slayer',
+        year: 2020,
+        nation: 'Japan',
+        director: 'Haruo Sotozaki',
+        image: './assets/images/poster8.jpg',
+        description: 'Demon Slayer is a Japanese animated film based on the manga series. It follows the story of a young boy named Tanjiro Kamado who becomes a demon slayer after his family is slaughtered, and he embarks on a quest for revenge.'
+    },
+    {
+        id: 9,
+        name: 'Inception',
+        year: 2010,
+        nation: 'USA',
+        director: 'Christopher Nolan',
+        image: './assets/images/poster9.jpg',
+        description: 'Inception is a science fiction film directed by Christopher Nolan. It explores the concept of shared dreaming and follows a skilled thief who is tasked with planting an idea into the mind of a CEO through dream infiltration.'
+    },
+    {
+        id: 10,
+        name: 'The Lord of the Rings: The Return of the King',
+        year: 2003,
+        nation: 'USA',
+        director: 'Peter Jackson',
+        image: './assets/images/poster10.jpg',
+        description: 'The Lord of the Rings: The Return of the King is a fantasy adventure film and the final installment in The Lord of the Rings trilogy. It depicts the climactic battle for Middle-earth as the forces of good confront the dark lord Sauron.'
+    }
 ];
