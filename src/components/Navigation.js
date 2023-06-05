c65d3bb1aea600b5946b0571b68d5f903d57dc46
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-06-05 12:11:43 +0700

    Done dark,light mode layout

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,42 @@
-import { Component } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-export default class Navigation extends Component {
-    render() {
-        return (
-            <nav className="navbar">
-                <div className="container">
-                    <Link className="navbar-logo" to="/">Nét Phíc</Link>
-                    <ul className="navbar-menu">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                        <li><Link to="/movies">Movies</Link></li>
-                        <li><Link to="/new-releases">New Releases</Link></li>
-                        <li><Link to="/my-list">My List</Link></li>
-                    </ul>
-                    <div className="navbar-profile">
-                        <img src="./assets/images/avatar.jpg" alt="Profile" className="navbar-profile-img" />
-                    </div>
-                </div>
-            </nav>
+import { ThemeContext } from './ThemeContext';
 
-        )
-    }
+export default function Navigation() {
+    const { isEnabled, toggleState } = useContext(ThemeContext);
+
+    return (
+        <nav className={isEnabled ? 'navbar navbar-dark' : 'navbar navbar-light'}>
+            <div className="container">
+                <Link className="navbar-logo" to="/">
+                    Nét Phíc
+                </Link>
+                <ul className="navbar-menu">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/movies">Movies</Link>
+                    </li>
+                    <li>
+                        <Link to="/new-releases">New Releases</Link>
+                    </li>
+                    <li>
+                        <Link to="/my-list">My List</Link>
+                    </li>
+                </ul>
+                <div className="navbar-profile">
+                    <button onClick={toggleState}>Toggle Theme</button>
+                    <img
+                        src="./assets/images/avatar.jpg"
+                        alt="Profile"
+                        className="navbar-profile-img"
+                    />
+                </div>
+            </div>
+        </nav>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ThemeContextProvider from './components/ThemeContext';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeContextProvider>
+
   </React.StrictMode>
 );
 
