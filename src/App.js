1c44ebd66dd7182bf82498b6605e6e5148e6a9b3
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2023-07-17 12:46:41 +0700

    add App.js AuthorizedRoute

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Admin from './admin/Admin';
 import './assets/css/bootstrap.min.css';
 import './assets/css/style.css';
 import Error from './components/Error';
 import Footer from './components/Footer';
 import Index from './components/Index';
 import Navigation from './components/Navigation';
-import PrivateRoutes from './components/PrivateRoute';
-import Booking from './customer/Booking';
-import BookingConfirm from './customer/BookingConfirm';
-import BookingList from './customer/BookingList';
-import BookingSuccessfully from './customer/BookingSuccessfully';
 import Customer from './customer/Customer';
-import CustomerBirdList from './customer/CustomerBirdList';
 import CustomerDashboard from './customer/CustomerDashboard';
-import CustomerProfileSetting from './customer/CustomerProfileSetting';
-import DoctorDashboard from './doctor/DoctorDashboard';
-import AddBird from './customer/AddBird';
-import BlogList from './blog/BlogList';
-import BlogDetail from './blog/BlogDetail';
+import Staff from './staff/Staff';
+import StaffDashboard from './staff/StaffDashboard';
 
 function App() {
   return (
@@ -25,29 +17,49 @@ function App() {
       <Router>
         <Navigation />
         <Routes>
-          <Route element={<Index />} path='/' />
-          <Route element={<PrivateRoutes />}>
-            <Route element={<Customer />} path='customer' >
-              <Route element={<CustomerDashboard />} path='customer-dashboard' />
-              <Route element={<CustomerProfileSetting />} path='customer-profile-setting' />
-              <Route element={<CustomerBirdList />} path='customer-bird-list' />
-              <Route element={<AddBird />} path='add-bird' />
-            </Route>
-            <Route element={<BookingList />} path='booking-list' />
-            <Route element={<Booking />} path='booking' />
-            <Route element={<BookingConfirm />} path='booking-confirm' />
-            <Route element={<BookingSuccessfully />} path='booking-successfully' />
-            <Route element={<DoctorDashboard />} path='doctor-dashboard' />
-          </Route>
-          <Route element={<BlogList />} path='blog-list' />
-          <Route element={<BlogDetail />} path='blog-detail/:id' />
+          {/* Public Route */}
+          <Route path='/' element={<Index />} />
 
-          <Route element={<Error />} path='*' />
+          {/* Private Route */}
+          {/* Staff */}
+          <Route
+            path="/staff"
+            element={<AuthorizedRoute component={Staff} allowedRoles={["admin", "staff"]} />}
+          >
+            <Route element={<StaffDashboard />} path='staff-dashboard' />
+          </Route>
+
+          {/* Customer */}
+          <Route
+            path="/customer"
+            element={<AuthorizedRoute component={Customer} allowedRoles={["customer", "admin"]} />}
+          >
+            <Route element={<CustomerDashboard />} path='customer-dashboard' />
+          </Route>
+
+
+
+          <Route
+            path="/admin"
+            element={<AuthorizedRoute component={Admin} allowedRoles={["admin"]} />}
+          />
+          <Route path='*' element={<Error />} />
         </Routes>
         <Footer />
       </Router>
     </div>
   );
+}
+
+function AuthorizedRoute({ component: Component, allowedRoles, ...rest }) {
+  const storedLoginData = localStorage.getItem("loginData");
+  const userRole = storedLoginData ? JSON.parse(storedLoginData).user.role : null;
+
+  if (allowedRoles.includes(userRole)) {
+    return <Component {...rest} />;
+  } else {
+    return <Navigate to="/error" replace />;
+  }
 }
 
 export default App;
